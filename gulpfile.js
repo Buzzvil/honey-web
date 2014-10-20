170f3c9e81a57b857a97965ce2f36d11e962d984
Teddy Cross <tkazec@gmail.com>
2014-10-20 14:45:31 +0900

    [gulpfile] hash styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var aws = require("aws-sdk");
+var crypto = require("crypto");
 var fs = require("fs");
 var gulp = require("gulp");
 var hl = require("highland");
 var jade = require("gulp-jade");
 var less = require("gulp-less");
+var path = require("path");
+var zlib = require("zlib");
 
 var l10nObj = {};
+var fileObj = {};
 
-var l10nAll = function () {
-    return hl(Object.keys(l10nObj)).each(this);
+var l10nAll = function (next) {
+    hl(Object.keys(l10nObj))
+        .map(this)
+        .merge()
+        .once("end", next)
+        .stopOnError(next)
+        .resume();
 };
 
 gulp.task("i18n", function () {
@@ -27,20 +36,37 @@ gulp.task("styles", ["i18n"], l10nAll.bind(function (l10n) {
     
     return gulp.src("page/*.less")
         .pipe(less(opts))
+        .pipe(hl())
+        .map(function (file) {
+            var type = path.extname(file.path);
+            var name = path.basename(file.path, type);
+            var hash = crypto
+                .createHash("md5")
+                .update(file.contents)
+                .digest("hex")
+                .slice(0, 12);
+            
+            file.path = path.dirname(file.path) + "/" + name + "-" + hash + type;
+            fileObj[name + type.slice(1)] = "/" + file.relative;
+            
+            return file;
+        })
         .pipe(gulp.dest("dist/" + l10n));
 }));
 
 gulp.task("pages", ["i18n"], l10nAll.bind(function (l10n) {
-    var opts = {
-        locals: l10nObj[l10n]
-    };
+    var opts = {};
+    
+    opts.locals = l10nObj[l10n];
+    opts.locals.files = fileObj;
+    opts.locals.gakey = process.env.GANALYTICS_KEY;
     
     return gulp.src("page/!(global).jade")
         .pipe(jade(opts))
         .pipe(gulp.dest("dist/" + l10n));
 }));
 
-gulp.task("upload", ["default"], l10nAll.bind(function (l10n) {
+gulp.task("upload", ["styles", "pages"], l10nAll.bind(function (l10n) {
     
 }));
 

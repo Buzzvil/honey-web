f4d74b6ce19bec4711169e946f46817c3d49cf15
Teddy Cross <tkazec@gmail.com>
2014-10-27 14:48:26 +0900

    [gulpfile] final (add s3 and cf)

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,31 @@ var less = require("gulp-less");
 var path = require("path");
 var zlib = require("zlib");
 
+aws.config.update({
+    region: "ap-northeast-1"
+});
+
+var GZcall = hl.wrapCallback(zlib.gzip.bind(zlib));
+
+var S3call = hl.wrapCallback(function (opts, next) {
+    new aws.S3().putObject(opts, next);
+});
+
+var CFcall = hl.wrapCallback(function (opts, next) {
+    new aws.CloudFront().createInvalidation(opts, next);
+});
+
 var l10nObj = {};
 var fileObj = {};
 
 var l10nAll = function (next) {
     hl(Object.keys(l10nObj))
         .map(this)
+        .map(function (val) {
+            return hl(val).errors(hl.log);
+        })
         .merge()
         .once("end", next)
-        .stopOnError(next)
         .resume();
 };
 
@@ -36,7 +52,8 @@ gulp.task("styles", ["clean", "i18n"], l10nAll.bind(function (l10n) {
     var opts = {
         compress: true,
         globalVars: {
-            l10n: l10n
+            i: JSON.stringify("/i/"),
+            I: JSON.stringify("/i/" + l10n + "/")
         }
     };
     
@@ -58,10 +75,13 @@ gulp.task("styles", ["clean", "i18n"], l10nAll.bind(function (l10n) {
         .pipe(gulp.dest("dist/" + l10n));
 }));
 
-gulp.task("pages", ["clean", "i18n"], l10nAll.bind(function (l10n) {
-    var opts = {};
+gulp.task("pages", ["styles"], l10nAll.bind(function (l10n) {
+    var opts = {
+        locals: l10nObj[l10n]
+    };
     
-    opts.locals = l10nObj[l10n];
+    opts.locals.i = "/i/";
+    opts.locals.I = "/i/" + l10n + "/";
     opts.locals.files = fileObj;
     opts.locals.gakey = process.env.GANALYTICS_KEY;
     
@@ -74,13 +94,75 @@ gulp.task("pages", ["clean", "i18n"], l10nAll.bind(function (l10n) {
         .pipe(gulp.dest("dist/" + l10n));
 }));
 
-gulp.task("upload", ["styles", "pages"], l10nAll.bind(function (l10n) {
+gulp.task("upload", ["pages"], l10nAll.bind(function (l10n) {
+    var list = gulp.src("dist/" + l10n + "/*").pipe(hl());
     
+    var cc = {
+        "": "no-cache",
+        ".css": "public, max-age=2592000"
+    };
+    
+    var ct = {
+        "": "text/html; charset=UTF-8",
+        ".css": "text/css; charset=UTF-8"
+    };
+    
+    return list
+        .fork()
+        .pluck("contents")
+        .flatMap(GZcall)
+        .zip(list.fork())
+        .flatMap(function (zip) {
+            var type = path.extname(zip[1].path);
+            
+            return S3call({
+                Bucket: l10nObj[l10n].meta.bucket,
+                Key: zip[1].relative,
+                Body: zip[0],
+                CacheControl: cc[type],
+                ContentEncoding: "gzip",
+                ContentLanguage: l10nObj[l10n].meta.code,
+                ContentType: ct[type]
+            });
+        });
+}));
+
+gulp.task("publish", ["upload"], l10nAll.bind(function (l10n) {
+    return gulp.src("dist/" + l10n + "/!(*.*)")
+        .pipe(hl())
+        .collect()
+        .flatMap(function (arr) {
+            arr = arr.map(function (val) {
+                return "/" + val.relative;
+            });
+            
+            return CFcall({
+                DistributionId: l10nObj[l10n].meta.cloudkey,
+                InvalidationBatch: {
+                    CallerReference: "gulp" + Date.now(),
+                    Paths: {
+                        Quantity: arr.length,
+                        Items: arr
+                    }
+                }
+            });
+        });
 }));
 
 gulp.task("watch", function () {
-    gulp.watch(["page/*.less"], ["styles"]);
-    gulp.watch(["page/*.jade", "l10n/*.json"], ["pages"]);
+    gulp.watch(["page/*.*", "l10n/*.json"], ["pages"]);
+    
+    express()
+        .use(express.static("dist/" + process.env.L10N, {
+            index: "index",
+            setHeaders: function (res, file) {
+                if (!path.extname(file)) {
+                    res.type("html");
+                }
+            }
+        }))
+        .use("/i", express.static("../honeyscreen-website-media"))
+        .listen(9100);
 });
 
-gulp.task("default", ["styles", "pages"]);
+gulp.task("default", ["pages"]);

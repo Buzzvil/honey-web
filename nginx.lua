9b7a6cf381f232108a47a19009d494dd739ca238
Teddy Cross <tkazec@gmail.com>
2014-11-18 21:10:39 +0900

    [core] rewrite nginx lua script!

--- nginx.lua
+++ nginx.lua
@@ -1,38 +1,32 @@
-function choose(l10n)
-    return nginx.redirect("http://" .. l10n .. ".honeyscreen.com")
-end
+local options = {}
+local it, err = ngx.re.gmatch(
+    ngx.var.http_accept_language or "ko",
+    [[(\w+(?:-\w+)*)(?:\s*;\s*q\s*=\s*(\d+.?\d*))?]]
+)
 
+local l10nKey
 local l10nObj = {
-    "en" = true,
-    "ko" = true,
-    "ja" = true,
-    "zh-tw" = true
+    ["en"] = "en",
+    ["ko"] = "ko",
+    ["ja"] = "ja",
+    ["zh"] = "zh-tw"
 }
 
-local l10nStr = ngx.var.http_accept_language or "ko"
-
-local cleaned = ngx.re.sub(l10nStr, "^.*:", "")
-local options = {}
-local iterator, err = ngx.re.gmatch(cleaned, "\\s*([a-z]+(?:-[a-z])*)\\s*(?:;q=([0-9]+(.[0-9]*)?))?\\s*(,|$)", "i")
-
-for m, err in iterator do
-    local lang = m[1]
-    local priority = 1
-    
-    if m[2] ~= nil then
-        priority = tonumber(m[2])
-        if priority == nil then
-            priority = 1
-        end
-    end
-    
-    table.insert(options, {lang, priority})
+for m, err in it do
+    table.insert(options, {
+        string.lower(m[1]),
+        string.sub(m[1], 1, 2),
+        tonumber(m[2]) or 1
+    })
 end
 
-table.sort(options, function(a, b) return b[2] < a[2] end)
+table.sort(options, function (a, b)
+    return b[3] < a[3]
+end)
 
-for index, lang in pairs(options) do
-    if l10nObj[lang[1]] then
-        return choose(lang[1])
-    end
+for i, l10n in pairs(options) do
+    l10nKey = l10nObj[l10n[1]] or l10nObj[l10n[2]]
+    if l10nKey then break end
 end
+
+ngx.redirect("http://" .. (l10nKey or "ko") .. ".honeyscreen.com")

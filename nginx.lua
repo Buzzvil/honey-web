231e0fedf7f47bf92f0ef8f46b5dbe4ac64abbad
Teddy Cross <tkazec@gmail.com>
2017-01-17 17:22:05 +0900

    2017 Q1 (#2)
    - Update copyrights and remove CC license.
    - Update ko & en download links.
    - Update sign in links.
    - Update lua.

--- nginx.lua
+++ nginx.lua
@@ -1,17 +1,21 @@
+-- define locales
+local l10nKey
+local l10nObj = {
+    ["en"] = "en",
+    ["ko"] = "ko",
+    ["ja"] = "ja",
+    ["zh"] = "zh-tw",
+    ["zh-hk"] = "en",
+}
+
+-- parse options
 local options = {}
 local it, err = ngx.re.gmatch(
     ngx.var.http_accept_language or "ko",
     [[(\w+(?:-\w+)*)(?:\s*;\s*q\s*=\s*(\d+.?\d*))?]]
 )
 
-local l10nKey
-local l10nObj = {
-    ["en"] = "en",
-    ["ko"] = "ko",
-    ["ja"] = "ja",
-    ["zh"] = "zh-tw"
-}
-
+-- build options
 for m, err in it do
     table.insert(options, {
         string.lower(m[1]),
@@ -20,15 +24,18 @@ for m, err in it do
     })
 end
 
+-- sort options
 table.sort(options, function (a, b)
     return b[3] < a[3]
 end)
 
+-- find locale
 for i, l10n in pairs(options) do
     l10nKey = l10nObj[l10n[1]] or l10nObj[l10n[2]]
     if l10nKey then break end
 end
 
+-- load locale
 ngx.redirect(
     "http://" ..
     (l10nKey or "ko") ..
